6285f748c189b5a8ff150f787f6be25a313608c4
sravani <sravaniab28@gmail.com>
2023-08-17 21:32:15 +0530

    hello world script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,11 +4,11 @@ echo "please enter your username"
 
 read -s USERNAME
 
-echo "username entered is: $USERNAME"
+# echo "username entered is: $USERNAME"
 
 
 echo "please enter your pwd"
 
 read -s PASSWORD
 
-echo "pwd entered is: $PASSWORD"
+#echo "pwd entered is: $PASSWORD"
