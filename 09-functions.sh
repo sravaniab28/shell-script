00805edfa32ca850bcb3e4f61197ac724ac53ef9
sravani <sravaniab28@gmail.com>
2023-08-18 21:50:53 +0530

    changes

--- 09-functions.sh
+++ 09-functions.sh
@@ -14,10 +14,10 @@ VALIDATE(){
 
 if [ $1 -ne 0 ]
  then 
-    echo "$2.......$R failure $N"
+    echo -e "$2.......$R failure $N"
     exit 1
 else
-     echo "$2 ........$G success $N"
+     echo -e "$2 ........$G success $N"
 fi
 }
 
