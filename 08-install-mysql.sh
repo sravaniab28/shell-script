39e789c18c8852cdcc1e5b97624e772787be32c9
sravani <sravaniab28@gmail.com>
2023-08-18 20:02:41 +0530

    changes

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -5,10 +5,20 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
  echo "error: INSTALLATion cancel"
+ exit 1
 else
  echo " ALLOW THE INSTALLATION"
 
 fi
 
+yum install mysqlll -y
 
-yum install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation  of mysql is error"
+    exit 1
+else
+     echo "installation of mysql is success"
+
+fi
