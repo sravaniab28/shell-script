fc09f1c547d1d3a8275464b26cea2b3b7eb04dee
sravani <sravaniab28@gmail.com>
2023-08-18 21:49:39 +0530

    changes

--- 09-functions.sh
+++ 09-functions.sh
@@ -4,14 +4,20 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+
 VALIDATE(){
 
 if [ $1 -ne 0 ]
  then 
-    echo "$2.......failure"
+    echo "$2.......$R failure $N"
     exit 1
 else
-     echo "$2 ........success"
+     echo "$2 ........$G success $N"
 fi
 }
 
