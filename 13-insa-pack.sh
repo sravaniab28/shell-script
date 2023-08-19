9967515713b265dc91873061f1fd2f0cfacde5ca
sravani <sravaniab28@gmail.com>
2023-08-19 15:12:05 +0530

    changes

--- 13-insa-pack.sh
+++ 13-insa-pack.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
-
+DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
+G="\e[32m"
 N="\e[0m"
-
-
+Y="\e[33m"
+VALIDATE(){
+    if [ $1 -ne 0]
+    then 
+    echo -e "installing....$R failure $N"
+    exit
+    else
+    echo -e"installing ....$G success $N"
+    fi
+    
+}
 if [ $USERID -ne 0 ]
 then 
     echo -e "$R ERROR: please run with root access $N"
     exit 1
 fi
+
+for i in $@
+do 
+yum list installed $i &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+  echo "$i is not instaaled lets install"
+  yum install $i -y &>>$LOGFILE
+  VALIDATE $? "$i"
+else
+   echo -e "$Y $i is already installed $N"
+
+   fi
+ done
 
