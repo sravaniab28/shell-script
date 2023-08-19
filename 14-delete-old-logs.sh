33ceb7a3e293b09ccd5b22b99159cd8daacf46ab
sravani <sravaniab28@gmail.com>
2023-08-19 16:11:31 +0530

    changes

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -3,7 +3,7 @@
 
 APP_LOGS_DIR=/home/centos/app-logs
 
-DATE=$(date +%F : %H :%M :%S)
+DATE=$(date +%F:%H:%M:%S)
 LOGSDIR=/home/centos/shellscript-logs
 # /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
