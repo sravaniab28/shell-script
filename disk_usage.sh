599c0b456bd634ee38ce63091c47819af550300c
sravani <sravaniab28@gmail.com>
2023-08-22 17:54:55 +0530

    changes

--- disk_usage.sh
+++ disk_usage.sh
@@ -9,12 +9,18 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-DISK_USAGE=$(df -hT | grep -v tmpfs)
+DISK_USAGE=$(df -hT | grep -vE tmpfs)
 DISK_USAGE_THRESHOLD=1
 message=""
 
 #IFS= means internal feild seperator is space.
 while IFS= read line
 do  
-  echo "output: $line"
+  USAGE=$(echo $line awk '{print $6}'| cut -d % -f1
+  partition=$(echo $line | awk '{print $1}')
+  if [ $usage -gt $DISK_USAGE_THRESHOLD ];
+  then
+     messages+="HIGH DISK USAGE ON $partition: $usage"
+ fi
 done <<<$DISK_USAGE
+echo "messages:$messages
